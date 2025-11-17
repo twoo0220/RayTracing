164959d73eb5c4e9d1ee12f4285d0ba76245994a
twoo0220 <twoo0220@gmail.com>
2025-11-18 00:41:33 +0900

    25.11.18

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RayTracing
 Study of the RayTracing in Onew Weekend series of books 
+
+## Environments
+
+- Windows 11
+- Visual Studio 2022 v143
+	- ISO C++14 Standard (Default)
---
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    int imageWidth = 256;
+    int imageHeight = 256;
+
+    std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+    for (int j = 0; j < imageHeight; ++j)
+    {
+        for (int i = 0; i < imageWidth; ++i)
+        {
+            double red = static_cast<double>(i) / static_cast<double>(imageWidth - 1);
+            double green = static_cast<double>(j) / static_cast<double>(imageHeight - 1);
+            double blue = 0.0;
+
+            int ired = static_cast<int>(255.999 * red);
+            int igreen = static_cast<int>(255.999 * green);
+            int iblue = static_cast<int>(255.999 * blue);
+
+            std::cout << ired << ' ' << igreen << ' ' << iblue << '\n';
+        }
+    }
+
+    return 0;
+}
