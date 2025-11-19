d7c4593be80150beb6d732eeb66b7551c91740c4
twoo0220 <twoo0220@gmail.com>
2025-11-20 00:33:09 +0900

    25.11.20

--- RayTracing/RayTracing/vec3.h
+++ RayTracing/RayTracing/vec3.h
@@ -32,6 +32,34 @@ public:
 		}
 	}
 
+	vec3& operator*=(double t)
+	{
+		element[0] *= t;
+		element[1] *= t;
+		element[2] *= t;
+		return *this;
+	}
+
+	vec3& operator/=(double t)
+	{
+		if (t == 0.0)
+		{
+			t = 1.0;
+		}
+		return *this *= 1 / t;
+	}
+
+	double length() const { return std::sqrt(length_squared()); }
+	double length_squared() const { return element[0] * element[0] + element[1] * element[1] + element[2] * element[2]; }
+
 private:
 	double element[3] = { 0, 0, 0 };
 };
+
+using point3 = vec3;
+
+// Vector Utility Functions
+inline std::ostream& operator<<(std::ostream& out, const vec3& v)
+{
+	return out << v.x() << ' ' << v.y() << ' ' << v.z();
+}
