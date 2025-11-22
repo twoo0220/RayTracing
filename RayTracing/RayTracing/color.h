3d9cdb82794ddde05a065fe125ac3cd0b3c5227b
twoo0220 <twoo0220@gmail.com>
2025-11-23 01:22:32 +0900

    25.11.23

---
+++ RayTracing/RayTracing/color.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "vec3.h"
+
+void writeColor(std::ostream& out, const vec3& pixelColor)
+{
+	double red = pixelColor.x();
+	double green = pixelColor.y();
+	double blue = pixelColor.z();
+
+	// [0, 1] 범위의 성분 값을 [0, 255] 바이트 범위로 변환
+	int redByte = static_cast<int>(255.999 * red);
+	int greenByte = static_cast<int>(255.999 * green);
+	int blueByte = static_cast<int>(255.999 * blue);
+
+	out << redByte << ' ' << greenByte << ' ' << blueByte << '\n';
+}
